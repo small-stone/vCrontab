836d4dc9e99d348597d66c20b22c27aa572055a0
小石头 <342206015@qq.com>
2020-05-25 23:32:51 +0800

    feat:新增隐藏参数
    feat:新增隐藏参数

--- README.md
+++ README.md
@@ -1,26 +1,33 @@
-# vue-crontab
-vue 的 cron 组件，支持解析/反解析 cron 表达式，生成最近五次的符合条件时间，依赖 vue2 和 element-ui
-[demo](https://small-stone.github.io/vCrontab/dist/)
+# [demo](https://small-stone.github.io/vCrontab/dist/)
+
 ![](public/demo.png)
 
+# vue-crontab
+
+vue 的 cron 组件，支持解析/反解析 cron 表达式，生成最近五次的符合条件时间，依赖 vue2 和 element-ui
+
 ## 安装方式
+
 ```
 npm install vcrontab
 ```
 
 ## 引入方式
+
 ```javascript
 //全局引入
-import vcrontab from 'vcrontab'
-Vue.use(vcrontab);//使用方式：<vcrontab></vcrontab>
+import vcrontab from "vcrontab";
+Vue.use(vcrontab); //使用方式：<vcrontab></vcrontab>
 
 //单独引入
-import vcrontab from 'vcrontab'
+import vcrontab from "vcrontab";
 export default {
-    components: { vcrontab }
-}
+  components: { vcrontab },
+};
 ```
+
 ## 代码示例
+
 ```javascript
 <template>
     <div id="app">
@@ -57,16 +64,20 @@ export default {
     }
 };
 </script>
-
 ```
 
 ## 参数
+
 - expression
-传入的 cron 表达式，可以反解析到 UI 上
+  传入的 cron 表达式，可以反解析到 UI 上
+
+- hideComponent
+  需要隐藏的组件数组，依次为`['second','min','hour','day','mouth','week','year']`
 
 ## 方法
-- fill
-点击确定时，把选择好的值返回。
 
-- hide 
-关闭组件时的回调
+- fill
+  点击确定时，把选择好的值返回。
+
+- hide
+  关闭组件时的回调
--- demo/App.vue
+++ demo/App.vue
@@ -1,51 +1,52 @@
 <template>
-    <div id="app">
-        <div class="box">
-            <el-input v-model="input" placeholder class="inp"></el-input>
-            <el-button type="primary" @click="showDialog">生成 cron</el-button>
-        </div>
-        <el-dialog title="生成 cron" :visible.sync="showCron">
-            <vcrontab @hide="showCron=false" @fill="crontabFill" :expression="expression"></vcrontab>
-        </el-dialog>
+  <div id="app">
+    <div class="box">
+      <el-input v-model="input" placeholder class="inp"></el-input>
+      <el-button type="primary" @click="showDialog">生成 cron</el-button>
     </div>
+    <el-dialog title="生成 cron" :visible.sync="showCron">
+      <vcrontab
+        @hide="showCron=false"
+        @fill="crontabFill"
+        :expression="expression"
+      ></vcrontab>
+    </el-dialog>
+  </div>
 </template>
 
 <script>
 import vcrontab from "../src/components/Crontab.vue";
 export default {
-    components: { vcrontab },
-    data() {
-        return {
-            input: "",
-            expression: "",
-            showCron: false
-        };
+  components: { vcrontab },
+  data() {
+    return {
+      input: "",
+      expression: "",
+      showCron: false,
+      hideComponent: ["second"],
+    };
+  },
+  methods: {
+    crontabFill(value) {
+      this.input = value;
     },
-    methods: {
-        crontabFill(value) {
-            this.input = value;
-        },
-        showDialog() {
-            this.expression = this.input;
-            " this.expression ", this.expression;
-            this.showCron = true;
-        }
-    }
+    showDialog() {
+      this.expression = this.input;
+      " this.expression ", this.expression;
+      this.showCron = true;
+    },
+  },
 };
 </script>
 
 <style>
 .box {
-    width: 500px;
-    height: 100px;
-    margin: 150px auto 0;
+  width: 500px;
+  height: 100px;
+  margin: 150px auto 0;
 }
 .inp {
-    width: 300px !important;
-    margin-right: 20px;
+  width: 300px !important;
+  margin-right: 20px;
 }
 </style>
-
-
-
-
